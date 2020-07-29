19cf8225309d958a09cc716e801c721f161507a1
hrshojaie <hrshojaie@gmail.com>
2020-07-29 17:55:41 +0430

    Add project files.

---
+++ WebApi/Controllers/MovieController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController 
+        : ControllerBase
+    {
+
+        public IActionResult Get()
+        {
+            var client = new MongoClient(
+                "mongodb://localhost:27017");
+
+            var db = client.GetDatabase("mymoviesdb");
+            var movies = db.GetCollection<Movie>("movies");
+            var moviesList = movies.Find(FilterDefinition<Movie>.Empty).ToList();
+
+            return Ok(moviesList);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Movie model)
+        {
+            var client = new MongoClient(
+                "mongodb://localhost:27017");
+
+            var db = client.GetDatabase("mymoviesdb");
+            var movies = db.GetCollection<Movie>("movies");
+
+            movies.InsertOne(model);
+
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Remove([FromBody] Movie model)
+        {
+            var client = new MongoClient(
+                "mongodb://localhost:27017");
+
+            var db = client.GetDatabase("mymoviesdb");
+            var movies = db.GetCollection<Movie>("movies");
+            movies.DeleteOne(a => a.Id == model.Id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Movie model)
+        {
+            var client = new MongoClient(
+                "mongodb://localhost:27017");
+
+            var db = client.GetDatabase("mymoviesdb");
+            var movies = db.GetCollection<Movie>("movies");
+            movies.ReplaceOne(a => a.Id == model.Id, model);
+            return Ok();
+        }
+    }
+}
---
+++ WebApi/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace WebApi.Models
+{
+    public class Movie
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("year")]
+        public int Year { get; set; }
+    }
+}
